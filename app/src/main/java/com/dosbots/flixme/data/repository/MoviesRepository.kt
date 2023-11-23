895e7a9455178460f39f367d4dc8e7d8b95ba811
German Krajcar <germankrajcar@gmail.com>
2023-11-23 20:32:24 -0300

    Move pager repository logic to a separate method

--- app/src/main/java/com/dosbots/flixme/data/repository/MoviesRepository.kt
+++ app/src/main/java/com/dosbots/flixme/data/repository/MoviesRepository.kt
@@ -4,10 +4,12 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import androidx.paging.PagingSource
 import com.dosbots.flixme.data.dabase.MoviesDao
 import com.dosbots.flixme.data.models.Movie
 import com.dosbots.flixme.data.models.database.PredefinedMoviesList
 import com.dosbots.flixme.data.repository.paging.PopularMoviesRemoteMediator
+import com.dosbots.flixme.data.repository.paging.PredefinedListsRemoteMediator
 import com.dosbots.flixme.data.repository.paging.TopRatedMoviesRemoteMediator
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -25,28 +27,28 @@ class MoviesRepositoryImpl @Inject constructor(
 ) : MoviesRepository {
 
     override fun getPopularMovies(): Flow<PagingData<Movie>> {
-        return Pager(
-            config = PagingConfig(
-                pageSize = MOVIES_PAGE_SIZE,
-                prefetchDistance = MOVIES_PREFETCH_DISTANCE
-            ),
-            remoteMediator = popularMoviesRemoteMediator
-        ) {
+        return moviesPager(popularMoviesRemoteMediator) {
             moviesDao.getMoviesInPredefinedListPaginated(PredefinedMoviesList.PopularMovies.name)
-        }.flow
+        }
     }
 
     override fun getTopRatedMovies(): Flow<PagingData<Movie>> {
-        return Pager(
-            config = PagingConfig(
-                pageSize = MOVIES_PAGE_SIZE,
-                prefetchDistance = MOVIES_PREFETCH_DISTANCE
-            ),
-            remoteMediator = topRatedMoviesRemoteMediator
-        ) {
+        return moviesPager(topRatedMoviesRemoteMediator) {
             moviesDao.getMoviesInPredefinedListPaginated(PredefinedMoviesList.TopRatedMovies.name)
-        }.flow
+        }
     }
+
+    private fun moviesPager(
+        remoteMediator: PredefinedListsRemoteMediator,
+        pagingSourceFactory: () -> PagingSource<Int, Movie>
+    ) = Pager(
+        config = PagingConfig(
+            pageSize = MOVIES_PAGE_SIZE,
+            prefetchDistance = MOVIES_PREFETCH_DISTANCE
+        ),
+        remoteMediator = remoteMediator,
+        pagingSourceFactory = pagingSourceFactory
+    ).flow
 }
 
 private const val MOVIES_PAGE_SIZE = 20
